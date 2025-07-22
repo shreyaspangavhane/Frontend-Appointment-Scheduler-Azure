66eb5506882f8583a9362ab5b138b15d519a3f6c
shreyaspangavhane <shreyaspangavhane2022@gmail.com>
2025-07-22 22:37:40 +0530

    Add homepage for Azure deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
@@ -18,16 +19,22 @@ function App() {
         <
         Route path = "/signup"
         element = { < Signup / > }
-        /> <
-        Route path = "/login"
-        element = { < Login / > }
-        /> <
+        // /> 
+        // 
+
+        <
+        Route path = "/"
+        element = { < Navigate to = "/login" / > }
+        />
+
+
+        <
         Route path = "/dashboard"
         element = { < Dashboard / > }
-        /> <
-        /Routes> <
-        /div> <
-        /Router>
+        /> < /
+        Routes > <
+        /div> < /
+        Router >
     );
 }
 
